3660962738d3736cc82f2356010bfa22d22a602b
AYOUB <127811483+AyoubKOURDI@users.noreply.github.com>
2023-04-24 19:26:42 +0100

    Vfinal

--- src/components/Acceuil.js
+++ src/components/Acceuil.js
@@ -5,8 +5,8 @@ const Acceuil = () => {
     <div className="container" id='border'>
         <p id='nom'>Ayoub KOURDI</p>
     <p className="description">
-      Ingénieur d'État en Informatique diplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatiqu'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également profen Informatique diplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également professeur en diplômé de l'ENSIAS ene d'un doctorat en informatique obtenu à l'Univel'Université Cadi Ayyad en 2017, est également professeur en diplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également profet professeur en diplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également professeur en idiplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également diplômé.
-rofesseur en idiplômé de l'ENSIAS en 2009 et titulaire d'un doctorat en informatique obtenu à l'Université Cadi Ayyad en 2017, est également diplômé.
+      Ingénieur d'État en Informatique diplômé de l'EMSI en 2024 et titulaire d'un diplome 'OFFSHORING' en informatique 
+      obtenu à l'Institut Specialisé de Technologie Appliquée a Marrakech en 2021.
     </p>
     </div>
   );
--- src/components/Projets.js
+++ src/components/Projets.js
@@ -9,22 +9,22 @@ function Projets() {
 
                 <li>
                     <h4>Projet 1 Kourdi Ayoub</h4>
-                    <p>Calculator Application - C++
-                        Project developed at College André Grasset, at Montreal, Canada.
-
-                        Simple Calculator coded in C++ with Visual Studio Community.
-
-                        The application allows simple calculations of integers and floating point numbers.</p>
-                    <a href='https://github.com/cbhering/Calculatrice-en-Cpp.git'>Voir le Projet</a>
+                    <p>Calcule Monaitaire
+                        Project pour le calcule different des convertions d'argent : 'Euro vers Mad' 'Mad vers Euro' | 'Mad vers Dollard' 'Dollard vers Mad' |
+                        'Dollard vers Euro' 'Euro vers Dollard' .</p>
+                    <a href='https://github.com/AyoubKOURDI/Projet_1.git'>Voir le Projet</a>
                 </li>
 
                 <li>
                     <h4>Projet 2 Kourdi Ayoub</h4>
-                    <p>Projet d'école qui a pour objectif de créer un showroom disponible en ligne. Pour plus de détails, le cahier des charges est disponible dans le répertoire "documents".
-                        La gestion de la base de données utilisé est MariaDB.
-                        La partie front-end est réalisé sur mon temps personnel sans Frameworks ou Libraries.
-                        Le sujet choisi est la photographie.</p>
-                    <a href='https://github.com/Noe-p/Developpement-application-web.git'>Voir le Projet</a>
+                    <p>Projet de Pockemon
+                        Ce projet utilise les differents API et les adaptes pour une application Mobile , avec le language 'JAVA' :
+                        eng description : PokéAPI is free and open to use. It is also very popular. Because of this, we ask every
+                        developer to abide by our fair use policy. People not complying with the fair use policy will have their
+                        IP address permanently banned.
+                        PokéAPI is primarily an educational tool, and we will not tolerate denial of service attacks preventing people from learning.
+                    </p>
+                    <a href='https://github.com/AyoubKOURDI/api_pokemon.git'>Voir le Projet</a>
                 </li>
 
             </ul>
